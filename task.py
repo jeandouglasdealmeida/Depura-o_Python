67792a18244cf6e70365103bda6868b3fa68ba08
jeandouglasdealmeida <47433794+jeandouglasdealmeida@users.noreply.github.com>
2019-09-12 13:38:50 -0300

    Add files via upload

---
+++ task.py
@@ -0,0 +1,9 @@
+from statistics import mean
+import pdb
+mean ([5, 5, 6, 6, 10])
+pdb.set_trace()
+nome = str(input("Digite um nome de aluno"))
+notas = []
+for x in range (0,5):
+	nota = float(input("digite uma nota:"))
+	notas.append(nota)
